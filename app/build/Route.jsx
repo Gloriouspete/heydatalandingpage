c5075ee3488485c044f1d076e24fee427732a212
Glö <105151046+Gloriouspete@users.noreply.github.com>
2023-10-21 20:33:40 +0100

    Create Route.jsx

---
+++ app/build/Route.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {BrowserRouter as Router,Route } from 'react-router-dom';
+
+import Home from '../layout/home/Home';
+
+const Routes = () =>{
+
+
+
+    return(
+
+        <>
+
+        <Router>
+
+            <Route path='/' exact Component={Home} />
+
+        </Router>
+
+        </>
+
+    )
+
+}
+
+export default Routes;

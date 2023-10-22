5d374eef1a0c6ce41c696e70e26a6ca4c03b2168
Glö <105151046+Gloriouspete@users.noreply.github.com>
2023-10-22 14:23:54 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,29 @@
-'use client'
-import React from 'react';
-import Home from './layout/home/Home';
-import './fonts/font.css';
 
-const App = () => {
+'use client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import './fonts/font.css';
+import Home from './layout/home/Home';
+import Login from './build/login/login';
+import Signup from './build/signup/signup';
+import Hosthall from './layout/hosthall/hosthall'
+import Joinhall from './layout/joinhall/joinhall';
+
+const App =() => {
   return (
-    <Home />
+    <>
+      <Router>
+        <Routes >
+          <Route path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+          
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
 export default App;
-

571bf1050a3c9db038783b728803f5dbed7167b2
Glö <105151046+Gloriouspete@users.noreply.github.com>
2023-10-22 15:24:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/app/layout/home/Home', // Adjust the path to your App component
-      },
-    ];
-  },
+  // ...
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/app/layout/home/Home', // Adjust the path to your App component
+  //     },
+  //   ];
+  // },
 };

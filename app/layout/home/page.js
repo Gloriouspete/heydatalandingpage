5e36d16d6730cf95eae59dece6d62ff22bc69e18
Glö <105151046+Gloriouspete@users.noreply.github.com>
2023-10-22 15:39:52 +0100

    Update page.js

--- app/layout/home/page.js
+++ app/layout/home/page.js
@@ -3,9 +3,9 @@ import React from 'react';
 import './media.css';
 import './style.css';
 
-import { Header } from '../../build/header/header';
-import Content from '../../build/content/content';
-import Footer from '../../build/footer/footer.tsx';
+import { Header } from '../../build/header/page';
+import Content from '../../build/content/page';
+import Footer from '../../build/footer/page.tsx';
 const Home = () => {
     return (
         <>

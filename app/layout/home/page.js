27e112956cb4de673fa35f3dd284b11b8584cce1
Glö <105151046+Gloriouspete@users.noreply.github.com>
2023-10-22 15:33:34 +0100

    Update page.js

--- app/layout/home/page.js
+++ app/layout/home/page.js
@@ -1,1 +1,20 @@
 
+import React from 'react';
+import './media.css';
+import './style.css';
+
+import { Header } from '../../build/header/header';
+import Content from '../../build/content/content';
+import Footer from '../../build/footer/footer.tsx';
+const Home = () => {
+    return (
+        <>
+            <div className='body'>
+               
+                <Content />
+                <Footer />
+            </div>
+        </>
+    )
+}
+export default Home;
